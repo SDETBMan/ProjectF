ba10854991a0216044876696d38cc49276a661c3
Brian <Brian@10.0.0.67>
2024-01-03 19:17:22 -0700

    Initial Commit

---
+++ src/f/DemoF.java
@@ -0,0 +1,12 @@
+package f;
+
+public class DemoF {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Sprint 1...");
+		System.out.println("Sprint 2...");
+		System.out.println("Sprint 3...");
+	}
+
+}
